ebc14204e847b6540c43f825fa8f9146708e6f52
Gabriel Naves <gabri.navess@gmail.com>
2021-10-02 15:43:17 -0300

    Temporary scenario, added player groundcheck and ball sprite

---
+++ Assets/Utility/Groundcheck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Groundcheck : MonoBehaviour {
+
+    [ViewOnly] public bool onGround;
+    [ViewOnly] public Collider2D floor;
+
+    public Vector3 position { get { return transform.position; } }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        onGround = true;
+        floor = collision;
+    }
+
+    void OnTriggerStay2D(Collider2D collision) {
+        onGround = true;
+        floor = collision;
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        onGround = false;
+    }
+
+    void LateUpdate() {
+        if (floor == null)
+            onGround = false;
+        else if (!floor.isActiveAndEnabled)
+            onGround = false;
+    }
+}
